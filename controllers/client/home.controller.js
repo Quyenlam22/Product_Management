ca562c0d542dfeca83212858633eec8d454212bf
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2024-11-25 08:30:07 +0700

    Add featured products and new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,27 @@
+const Product = require("../../models/product.model")
+
+const productsHelper = require("../../helpers/product")
+
 // [GET] /
 module.exports.index = async (req, res) => {
+    const productsFeatured = await Product.find({
+        featured: "1",
+        status: "active",
+        deleted: false
+    }).limit(6)
+
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured)
+
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({position: "desc"}).limit(6)
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
+
     res.render('client/page/home/index.pug', { 
         pageTitle: 'Trang chủ',
-        
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     })
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,19 +1,15 @@
 const Product = require("../../models/product.model")
 
+const productsHelper = require("../../helpers/product")
+
 // [GET] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
         deleted: false
-    }).sort({position: "desc"})
+    }).sort({position: "asc"})
 
-
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(2)
-        return item
-    })
-    req.flash("success", "Không tìm thấy sản phẩm!")
-
+    const newProducts = productsHelper.priceNewProducts(products)
 
     res.render(`client/page/products/index`, {
         pageTitle: "Danh sách sản phẩm",
