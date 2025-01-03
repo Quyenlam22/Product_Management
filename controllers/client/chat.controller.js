e47345d21d02246b8370e8f24b104c3bda5f9765
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-01-03 17:04:37 +0700

    Typing Chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -23,6 +23,15 @@ module.exports.index = async (req, res) => {
                 content: content
             })
         })
+
+        // Typing
+        socket.on("CLIENT_SEND_TYPING", async (type) => {
+            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+                fullName: fullName,
+                userId: userId,
+                type: type
+            })
+        })
     })
 
     const chats = await Chat.find({
--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,9 @@ var socket = io();
 
 import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 
+const body = document.querySelector(".chat .inner-body")
+const elementListTyping = document.querySelector(".chat .inner-list-typing")
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form")
 if (formSendData) {
@@ -9,9 +12,11 @@ if (formSendData) {
     formSendData.addEventListener("submit", (e) => {
         e.preventDefault()
         const content = e.target.elements.content.value
+        // const content = contentInput.value
         if (content) {
             socket.emit("CLIENT_SEND_MESSAGE", content)
             e.target.elements.content.value = ""
+            socket.emit("CLIENT_SEND_TYPING", "hidden")
         }
         // console.log(contentInput.value)
     })
@@ -19,7 +24,6 @@ if (formSendData) {
 
 // SERVER_RETURN_MESSAGE
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
-    const body = document.querySelector(".chat .inner-body")
     const myId = document.querySelector("[my-id]").getAttribute("my-id")
 
     const div = document.createElement("div")
@@ -36,15 +40,14 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
         ${htmlFullName}
         <div class="inner-content">${data.content}</div>
     `
-    body.appendChild(div)
+    body.insertBefore(div, elementListTyping)
 
     body.scrollTop = body.scrollHeight
 })
 
 // Scroll Chat to Bottom
-const bodyChat = document.querySelector(".chat .inner-body")
-if (bodyChat) {
-    bodyChat.scrollTop = bodyChat.scrollHeight
+if (body) {
+    body.scrollTop = body.scrollHeight
 }
 
 // Emoji Picker 
@@ -63,13 +66,68 @@ if (buttonIcon) {
     }
 }
 
+// Show Typing
+var timeOut
+const showTyping = () => {
+    socket.emit("CLIENT_SEND_TYPING", "show")
+
+    clearTimeout(timeOut)
+
+    timeOut = setTimeout(() => {
+        socket.emit("CLIENT_SEND_TYPING", "hidden")
+    }, 3000)
+}
+
 // Insert Icon
 const emojiPicker = document.querySelector("emoji-picker")
-if(emojiPicker){
+if (emojiPicker) {
     const inputChat = document.querySelector(".chat .inner-form input[name='content']")
 
     emojiPicker.addEventListener('emoji-click', (e) => {
         const icon = e.detail.unicode
         inputChat.value = inputChat.value + icon
-    }) 
+
+        inputChat.setSelectionRange(inputChat.value.length, inputChat.value.length)            
+        inputChat.focus()
+        showTyping()
+    })
+
+    // Input Keyup
+    inputChat.addEventListener("keyup", () => {
+        showTyping()
+    })
+}
+
+
+//SERVER_RETURN_TYPING
+
+if (elementListTyping) {
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+
+        const existTyping = elementListTyping.querySelector(`[user-id="${data.userId}"]`)
+        if (data.type == "show") {
+            if (!existTyping) {
+                const boxTyping = document.createElement("div")
+                boxTyping.classList.add("box-typing")
+                boxTyping.setAttribute("user-id", data.userId)
+
+                boxTyping.innerHTML = `
+                <div class="inner-name"> ${data.fullName} </div>
+                <div class="inner-dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                `
+                elementListTyping.appendChild(boxTyping)
+            }
+            
+            body.scrollTop = body.scrollHeight
+        }
+        else{
+            if(existTyping){
+                elementListTyping.removeChild(existTyping)
+            }
+        }
+    })
 }
